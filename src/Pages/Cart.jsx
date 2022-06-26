bb561060bc9b4c384f6f544df00c7cbab31a9149
Audrius <darkinforest@gmail.com>
2022-06-26 20:07:13 +0300

    added title to Cart page

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,10 +1,15 @@
 import React from "react"
+import Container from "../components/Container/Container"
 import Footer from "../components/Footer/Footer"
+import Title from "../components/Title/Title"
 
 const Cart = () => {
   return (
     <>
-      <div>Cart</div> <Footer />
+      <Container>
+        <Title title='Cart' />
+      </Container>
+      <Footer />
     </>
   )
 }
